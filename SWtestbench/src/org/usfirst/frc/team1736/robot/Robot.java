dd8a827b2b43379d81426370928ab276d3ddc1da
Chris Gerth <chrisgerth010592@gmail.com>
2016-10-27 19:07:38 -0500

    explicitly defined PDP channel

--- SWtestbench/src/org/usfirst/frc/team1736/robot/Robot.java
+++ SWtestbench/src/org/usfirst/frc/team1736/robot/Robot.java
@@ -29,7 +29,7 @@ public class Robot extends IterativeRobot {
      * initialization code.
      */
     public void robotInit() {
-        pdp = new PowerDistributionPanel();
+        pdp = new PowerDistributionPanel(0);
         webserver = new CasseroleWebServer();
         bpe = new BatteryParamEstimator(100);
 
